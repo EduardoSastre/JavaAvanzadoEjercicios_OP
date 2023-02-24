92d96b1126a5b1229122a917900f5ae7ea308ef8
Eduardo Sastre Justiniano <ing.eduardo.sastre@outlook.com>
2023-02-23 22:05:19 -0600

    Add Ejercicios 678

---
+++ src/EjerciciosSesiones678/Application.java
@@ -0,0 +1,32 @@
+package EjerciciosSesiones678;
+
+public class Application {
+
+      private static Application app;
+
+      boolean isRunning = false;
+
+      private Application(){}
+
+      public static Application getInstance() {
+
+            if ( app == null ) {
+                  app = new Application();
+            }
+
+            return app;
+
+      }
+
+      public void run(){
+
+            if (!isRunning){
+                  isRunning = true;
+                  System.out.println("Starts running...");
+            } else {
+                  System.out.println( "Already running..." );
+            }
+
+      }
+
+}
